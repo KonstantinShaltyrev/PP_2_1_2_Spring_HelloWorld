8b9b9b431589083199f5e60af1217f627253da50
KonstantinShaltyrev <ska1buzon@gmail.com>
2024-06-29 11:29:56 +0300

    v1.0

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,17 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean1 =
+                applicationContext.getBean("helloworld", HelloWorld.class);
+        Cat bean2 =
+                (Cat) applicationContext.getBean("cat");
+        Cat bean3 =
+                applicationContext.getBean(Cat.class);
         System.out.println(bean.getMessage());
+        System.out.println(bean1.getMessage());
+        System.out.println(bean2.toString());
+        System.out.println(bean3.toString());
+        System.out.println("helloworld beans comparison: " + (bean == bean1));
+        System.out.println("cat beans comparison: " + (bean2 == bean3));
     }
 }
